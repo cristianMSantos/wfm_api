02ec8936e9a5795a6c54c899375c209aa38b09bf
Cristian de Moraes Santos <cristian_acdc15@hotmail.com>
2022-12-05 09:04:27 -0300

    add_id_situacao_sisfin

--- app/Models/Colaborador.php
+++ app/Models/Colaborador.php
@@ -19,7 +19,7 @@ class Colaborador extends Model
         ->select('nome', 'matricula', 'f.de_funcao', 's.de_situacao', 'login', 'filial',
         'emp.co_funcao', 'dtnascimento', 'dtadmissao', 'emp.id_situacao', 'jorn_ent', 'jorn_sai', 'mat_gestor', 'mat_monitor')
         ->join('sc_bases.tb_funcao as f', 'emp.co_funcao', 'f.co_funcao')
-        ->join('sc_bases.tb_situacao as s', 'emp.id_situacao', 's.id_situacao')
+        ->join('sc_bases.tb_situacao as s', 'emp.id_situacao', 's.id_situacao_sisfin')
         ->get();
     }
 
