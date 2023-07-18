acfb915f0fa307d35446ec262dc8d347d0dc755b
cristian_santos <cristian_santos@git>
2023-07-18 10:08:34 -0300

    add_access_management

--- app/Models/View_Funcionarios.php
+++ app/Models/View_Funcionarios.php
@@ -10,9 +10,14 @@ class View_Funcionarios extends Model
     public $timestamps = false;
     protected $table = 'public.vw_funcionario';
     protected $primaryKey = 'matricula_pl';
-    
+
+    function contemSomenteNumeros($str) {
+        // Expressão regular para verificar se a string possui somente números
+        return preg_match('/^\d+$/', $str) === 1;
+    }
+      
     public function listFuncionarios($colaborador){
-        return View_Funcionarios::select('matricula_pl', 'no_operador', DB::raw("(
+        $result = View_Funcionarios::select('matricula_pl', 'no_operador', DB::raw("(
             CASE
               WHEN EXISTS (
                 SELECT 1 FROM tb_usuario WHERE matricula = matricula_pl
@@ -20,8 +25,16 @@ class View_Funcionarios extends Model
               THEN TRUE 
               ELSE FALSE
             END
-          ) AS has_access"))
-        ->where('no_operador', 'LIKE', '%'.$colaborador.'%')->where('id_situacao', 1)->orderBy('no_operador', 'asc')->get()->toArray();
+          ) AS has_access"));
+          
+          if($this->contemSomenteNumeros($colaborador)){
+            $result->where('matricula_pl', $colaborador);
+          }else{
+            $result->where('no_operador', 'LIKE', '%'.strtoupper($colaborador).'%');
+          }
+          
+        $result = $result->where('id_situacao', 1)->orderBy('no_operador', 'asc')->get()->toArray();
+        return $result;
     }
 
     public function getAuthUser(){
